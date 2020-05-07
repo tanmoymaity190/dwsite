eeeb58747eda1b1b54f1b1eabd461ef8bd0341ab
tanmoymaity190 <64913551+tanmoymaity190@users.noreply.github.com>
2020-05-07 23:07:29 +0530

    Add files via upload

---
+++ password.js
@@ -0,0 +1,15 @@
+function check(form)
+                    {
+                        if(form.name.value=="tanmoy" && form.password.value=="darkworld")
+                        {
+                            window.open("intro.html")
+                        }
+                        
+
+                        else
+                            {
+                            alert("Credentials are Invalid")
+                               return false;
+                             }
+                       
+                     }
